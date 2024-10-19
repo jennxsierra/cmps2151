ee055ca82807990ef9fb240ecfb2e1a8bcb27fad
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2024-10-18 19:33:53 -0600

    Added exercise 15

---
+++ exercises/15-document-obj-model/index.js
@@ -0,0 +1,7 @@
+// change the color of the google link
+const link = document.querySelector("a");
+link.style.color = "green";
+
+// change the color of h1
+const h1 = document.querySelector("h1");
+h1.style.color = "purple";
