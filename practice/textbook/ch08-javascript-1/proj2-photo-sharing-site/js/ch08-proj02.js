30533d806f31e5e38a18dd1ed55672e25bb6d0b8
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2024-10-23 00:37:51 -0600

    Finished ch08 project 2

--- practice/textbook/ch08-javascript-1/proj2-photo-sharing-site/js/ch08-proj02.js
+++ practice/textbook/ch08-javascript-1/proj2-photo-sharing-site/js/ch08-proj02.js
@@ -1,1 +1,37 @@
 /* add your code here */
+const photos = JSON.parse(content);
+
+const constructStyle = (color) => {
+  return `background-color: ${color.hex};${
+    color.luminance < 70 ? " color: white;" : ""
+  }`;
+};
+
+const constructColor = (color) => {
+  return `<span style="${constructStyle(color)}">${color.name}</span>`;
+};
+
+const outputColors = (colors) => {
+  for (let color of colors) {
+    document.write(constructColor(color));
+  }
+};
+
+const outputCard = (photo) => {
+  document.write(`
+    <article>
+      <img src="images/${photo.filename}" alt="${photo.title}">
+      <div class="caption">
+        <h2>${photo.title}</h2>
+        <p>${photo.location.city}, ${photo.location.country}</p>
+        <h3>Colors</h3>
+`);
+
+  outputColors(photo.colors);
+
+  document.write(`</div></article>`);
+};
+
+for (let photo of photos) {
+  outputCard(photo);
+}
