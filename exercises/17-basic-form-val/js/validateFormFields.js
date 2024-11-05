709e88a6033c5d3674eb646f3a29d66a0bb5dd64
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2024-11-04 19:06:18 -0600

    Restructured folders & finshed index1, index2 and index3

---
+++ exercises/17-basic-form-val/js/validateFormFields.js
@@ -0,0 +1,25 @@
+const inputField = document.querySelector("#value1");
+const errorOutput = document.querySelector("#errorOutput");
+const submitButton = document.querySelector('input[type="submit"]');
+
+function alphanumeric(inputText) {
+  const regex = /^[0-9a-zA-Z]+$/;
+
+  if (regex.test(inputText)) {
+    alert("Your registration number has accepted: you can try another.");
+    inputField.focus();
+    return true;
+  } else {
+    alert("Please input alphanumeric characters only.");
+    return false;
+  }
+}
+
+submitButton.addEventListener("click", (e) => {
+  if (!alphanumeric(inputField.value)) {
+    e.preventDefault();
+    errorOutput.textContent = "Please input alphanumeric characters only.";
+  } else {
+    errorOutput.textContent = "";
+  }
+});
